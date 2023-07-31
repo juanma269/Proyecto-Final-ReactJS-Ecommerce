99d25b98e2133cacc30ed635ceeced81b8bab059
Juan <jmponceguajardo@gmail.com>
2023-07-31 16:03:39 -0300

    funcionalidad de añadir carrito, limpiar carrito

--- src/pages/cart/CartContainer.jsx
+++ src/pages/cart/CartContainer.jsx
@@ -1,7 +1,26 @@
+import { useContext } from "react";
+import { CartContext } from "../../context/CartContext";
+import { Button } from "@mui/material";
+
 const CartContainer = () => {
+  const { cart, clearCart } = useContext(CartContext);
+
   return (
     <div>
       <h1>Esto es el carritoo</h1>
+      {cart.map((elemento) => {
+        return (
+          <div key={elemento.id}>
+            <h4>{elemento.title}</h4>
+            <h5>{elemento.price}</h5>
+            <h6>{elemento.quantity}</h6>
+          </div>
+        );
+      })}
+
+      <Button onClick={clearCart} color="secondary" variant="contained">
+        Limpiar carrito
+      </Button>
     </div>
   );
 };
--- src/pages/itemDetail/ItemDetail.jsx
+++ src/pages/itemDetail/ItemDetail.jsx
@@ -1,10 +1,12 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { products } from "../../productsMocks";
 import { useParams } from "react-router-dom";
 import { Container } from "@mui/material";
 import DetailProductCard from "../../common/detailProductCard/DetailProductCard";
+import { CartContext } from "../../context/CartContext";
 
 const ItemDetail = () => {
+  const { addToCart } = useContext(CartContext);
   const [producto, setProducto] = useState({});
   const { id } = useParams();
 
@@ -14,8 +16,8 @@ const ItemDetail = () => {
   }, [id]);
 
   const onAdd = (cantidad) => {
-    console.log(producto);
-    console.log(cantidad);
+    let productCart = { ...producto, quantity: cantidad };
+    addToCart(productCart);
   };
   return (
     <Container maxWidth="md">
