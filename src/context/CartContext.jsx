c428a3da22f0250871405df5a9802476c449e999
Juan <jmponceguajardo@gmail.com>
2023-08-01 16:48:28 -0300

    boton eliminar, funcion para que no se dupliquen las cards en carrito

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -4,13 +4,38 @@ export const CartContext = createContext();
 
 const CartContextProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
+
   const addToCart = (item) => {
-    setCart([...cart, item]);
+    const existe = isInCart(item.id);
+    if (existe) {
+      let newArray = cart.map((elemento) => {
+        if (elemento.id === item.id) {
+          return { ...elemento, quantity: elemento.quantity + item.quantity };
+        } else {
+          return elemento;
+        }
+      });
+      setCart(newArray);
+    } else {
+      setCart([...cart, item]);
+    }
   };
+
   const clearCart = () => {
     setCart([]);
   };
-  let data = { cart, addToCart, clearCart };
+
+  const deleteById = (id) => {
+    let newArray = cart.filter((elemento) => elemento.id !== id);
+    setCart(newArray);
+  };
+
+  const isInCart = (id) => {
+    let exist = cart.some((elemento) => elemento.id === id);
+    return exist;
+  };
+
+  let data = { cart, addToCart, clearCart, deleteById };
 
   return <CartContext.Provider value={data}>{children}</CartContext.Provider>;
 };
--- src/pages/cart/CartContainer.jsx
+++ src/pages/cart/CartContainer.jsx
@@ -4,17 +4,24 @@ import { Button } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
 
 const CartContainer = () => {
-  const { cart, clearCart } = useContext(CartContext);
+  const { cart, clearCart, deleteById } = useContext(CartContext);
 
   return (
     <div>
       <h1>Esto es el carritoo</h1>
       {cart.map((elemento) => {
         return (
-          <div key={elemento.id}>
+          <div key={elemento.id} style={{ border: "2px solid orange" }}>
             <h4>{elemento.title}</h4>
             <h5>{elemento.price}</h5>
             <h6>{elemento.quantity}</h6>
+            <Button
+              variant="outlined"
+              color="error"
+              onClick={() => deleteById(elemento.id)}
+            >
+              <DeleteIcon />
+            </Button>
           </div>
         );
       })}
