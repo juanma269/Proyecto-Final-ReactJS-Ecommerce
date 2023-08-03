51b19c27fbd479e4c632b46f7af54b306bb599b5
Juan <jmponceguajardo@gmail.com>
2023-08-03 13:31:59 -0300

    se añaden alertas de confirmacion y ventana antes de vaciar carrito

--- src/pages/cart/CartContainer.jsx
+++ src/pages/cart/CartContainer.jsx
@@ -2,13 +2,50 @@ import { useContext } from "react";
 import { CartContext } from "../../context/CartContext";
 import { Button } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
+import Swal from "sweetalert2";
 
 const CartContainer = () => {
   const { cart, clearCart, deleteById } = useContext(CartContext);
+  const limpiarCarrito = () => {
+    Swal.fire({
+      title: "¿Seguro que deseas limpiar tu carrito?",
+      background: "#424242",
+      color: "#fff",
+      iconColor: "#e65100",
+      icon: "question",
+      showDenyButton: true,
+      denyButtonColor: "#d50000",
+      confirmButtonText: "SI 🗑️",
+      confirmButtonColor: "#4caf50",
+      denyButtonText: `NO`,
+    }).then((result) => {
+      /* Read more about isConfirmed, isDenied below */
+      if (result.isConfirmed) {
+        clearCart();
+        Swal.fire({
+          background: "#424242",
+          color: "#fff",
+          iconColor: "#e65100",
+          title: "Carrito Limpio",
+          icon: "success",
+          confirmButtonColor: "#4caf50",
+        });
+      } else if (result.isDenied) {
+        Swal.fire({
+          background: "#424242",
+          color: "#fff",
+          iconColor: "#e65100",
+          title: "Sin cambios",
+          icon: "info",
+          confirmButtonColor: "#4caf50",
+        });
+      }
+    });
+  };
 
   return (
     <div>
-      <h1>Esto es el carritoo</h1>
+      <h1>Bienvenido a tu Carrito</h1>
       {cart.map((elemento) => {
         return (
           <div key={elemento.id} style={{ border: "2px solid orange" }}>
@@ -25,15 +62,18 @@ const CartContainer = () => {
           </div>
         );
       })}
-
-      <Button
-        variant="outlined"
-        color="secondary"
-        onClick={clearCart}
-        endIcon={<DeleteIcon />}
-      >
-        Limpiar carrito
-      </Button>
+      {cart.length == 0 ? (
+        <h2>Tu carrito está vacío</h2>
+      ) : (
+        <Button
+          variant="outlined"
+          color="secondary"
+          onClick={limpiarCarrito}
+          endIcon={<DeleteIcon />}
+        >
+          Limpiar carrito
+        </Button>
+      )}
     </div>
   );
 };
--- src/pages/itemDetail/ItemDetail.jsx
+++ src/pages/itemDetail/ItemDetail.jsx
@@ -4,6 +4,7 @@ import { useParams } from "react-router-dom";
 import { Container } from "@mui/material";
 import DetailProductCard from "../../common/detailProductCard/DetailProductCard";
 import { CartContext } from "../../context/CartContext";
+import Swal from "sweetalert2";
 
 const ItemDetail = () => {
   const { addToCart } = useContext(CartContext);
@@ -18,6 +19,17 @@ const ItemDetail = () => {
   const onAdd = (cantidad) => {
     let productCart = { ...producto, quantity: cantidad };
     addToCart(productCart);
+
+    Swal.fire({
+      position: "center",
+      background: "#424242",
+      color: "#fff",
+      icon: "success",
+      iconColor: "#e65100",
+      title: "Añadido al carrito",
+      showConfirmButton: false,
+      timer: 1500,
+    });
   };
   return (
     <Container maxWidth="md">
